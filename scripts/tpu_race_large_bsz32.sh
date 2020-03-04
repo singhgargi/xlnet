53024c21ff624be90e3642c6ab1eee373dc84e24
singhgargi <40858250+singhgargi@users.noreply.github.com>
2020-03-04 19:20:52 +0530

    Update tpu_race_large_bsz32.sh

--- scripts/tpu_race_large_bsz32.sh
+++ scripts/tpu_race_large_bsz32.sh
@@ -3,36 +3,28 @@
 #### local path
 RACE_DIR=data/RACE
 INIT_CKPT_DIR=xlnet_cased_L-24_H-1024_A-16
-
-#### google storage path
-GS_ROOT=
-GS_INIT_CKPT_DIR=${GS_ROOT}/${INIT_CKPT_DIR}
-GS_PROC_DATA_DIR=${GS_ROOT}/proc_data/race
-GS_MODEL_DIR=${GS_ROOT}/experiment/race
-
-# TPU name in google cloud
-TPU_NAME=
+PROC_DATA_DIR=proc_data/race
+MODEL_DIR=experiment/race
 
 python run_race.py \
-  --use_tpu=True \
-  --tpu=${TPU_NAME} \
-  --num_hosts=4 \
-  --num_core_per_host=8 \
+  --use_tpu=False \
+  --num_hosts=1 \
+  --num_core_per_host=1 \
   --model_config_path=${INIT_CKPT_DIR}/xlnet_config.json \
   --spiece_model_file=${INIT_CKPT_DIR}/spiece.model \
-  --output_dir=${GS_PROC_DATA_DIR} \
-  --init_checkpoint=${GS_INIT_CKPT_DIR}/xlnet_model.ckpt \
-  --model_dir=${GS_MODEL_DIR} \
+  --output_dir=${PROC_DATA_DIR} \
+  --init_checkpoint=${INIT_CKPT_DIR}/xlnet_model.ckpt \
+  --model_dir=${MODEL_DIR} \
   --data_dir=${RACE_DIR} \
   --max_seq_length=512 \
   --max_qa_length=128 \
   --uncased=False \
   --do_train=True \
-  --train_batch_size=32 \
+  --train_batch_size=4 \
   --do_eval=True \
   --eval_batch_size=32 \
-  --train_steps=12000 \
-  --save_steps=1000 \
+  --train_steps=85000 \
+  --save_steps=5000 \
   --iterations=1000 \
   --warmup_steps=1000 \
   --learning_rate=2e-5 \
